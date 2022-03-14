6eed8a2726fb0186643ac04ff30b5d3fa4b6f8c8
amaliagurau <94163502+amaliagurau@users.noreply.github.com>
2022-03-14 18:28:42 +0200

    Add files via upload

---
+++ Ex.5(lectia3)/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Suma.suma(2,6));
+		System.out.println(Suma.suma(2,6,7));
+		System.out.println(Suma.suma(2,6,5,9));
+
+	}
+
+}
